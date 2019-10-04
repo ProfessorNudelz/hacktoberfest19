2e6168f0687b810476505425df40287d61716591
Jesse Lyon <jesse.a.lyon+github@gmail.com>
2019-10-04 22:38:15 +1000

    Added contributor - test

--- contributors.js
+++ contributors.js
@@ -7,6 +7,13 @@ const data = {
       "city": "Melbourne",
       "country": "Australia"
     },
+    {
+      "name": "Jane",
+      "username": "JaneDoe",
+      "githubURL": "https://github.com/JaneDoe",
+      "city": "Las Vegas",
+      "country": "United States of America"
+    },
   ]
 }
 
